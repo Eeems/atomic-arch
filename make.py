152ab6c33d6f28390ba33de4d94395e1616b8194
Nathaniel van Diepen <nathaniel.vandiepen@gmail.com>
2025-05-02 17:49:22 -0600

    Split out rootfs

--- make.py
+++ make.py
@@ -4,6 +4,7 @@ import sys
 import os
 
 from typing import cast
+from datetime import datetime
 from importlib.util import spec_from_loader
 from importlib.util import module_from_spec
 from importlib.machinery import SourceFileLoader
@@ -67,47 +68,103 @@ def atomic_arch(*args: str, target: str | None = None):
     )
 
 
-def build_targets(args: argparse.Namespace) -> list[str]:
-    targets = cast(list[str], args.target)
-    if "base" in targets and targets[0] != "base":
-        targets.remove("base")
-
-    if "base" not in targets:
-        targets = ["base"] + targets
-
-    return targets
-
-
 def do_build(args: argparse.Namespace):
-    for target in build_targets(args):
+    if not is_root():
+        print("Must be run as root")
+        sys.exit(1)
+
+    for target in cast(list[str], args.target):
         build(target)
 
 
 def do_iso(args: argparse.Namespace):
-    for target in build_targets(args):
+    if not is_root():
+        print("Must be run as root")
+        sys.exit(1)
+
+    for target in cast(list[str], args.target):
         build(target)
         atomic_arch("build", target=target)
 
     for target in cast(list[str], args.target):
         atomic_arch("iso", target=target)
 
+def do_rootfs(args: argparse.Namespace):
+    noBuild = cast(bool, args.noBuild)
+    if not is_root() and not noBuild:
+        print("Must be run as root")
+        sys.exit(1)
+
+    containerfile = "variants/rootfs.Containerfile"
+    with open(containerfile, "r") as f:
+        lines = f.readlines()
+
+    config = {
+        x[0]: x[1]
+        for x in
+        [
+            x.rstrip().split(" ", 1)[1].split("=", 1)
+            for x in lines
+            if x.startswith("ARG ARCHIVE_YEAR=")
+            or x.startswith("ARG ARCHIVE_MONTH=")
+            or x.startswith("ARG ARCHIVE_DAY=")
+            or x.startswith("ARG PACSTRAP_TAG=")
+        ]
+    }
+
+    year = cast(str|None, args.year) or config["ARCHIVE_YEAR"]
+    month = cast(str|None, args.month) or config["ARCHIVE_MONTH"]
+    day = cast(str|None, args.day) or config["ARCHIVE_DAY"]
+    tag = cast(str|None, args.tag) or config["PACSTRAP_TAG"]
+
+    with open(containerfile, "w") as f:
+        for line in lines:
+            if line.startswith("ARG ARCHIVE_YEAR="):
+                line = f"ARG ARCHIVE_YEAR={year}\n"
+
+            elif line.startswith("ARG ARCHIVE_MONTH="):
+                line = f"ARG ARCHIVE_MONTH={month}\n"
+
+            elif line.startswith("ARG ARCHIVE_DAY="):
+                line = f"ARG ARCHIVE_DAY={day}\n"
+
+            elif line.startswith("ARG PACSTRAP_TAG="):
+                line = f"ARG PACSTRAP_TAG={tag}\n"
+
+            f.write(line)
+
+    if not noBuild:
+        build("rootfs")
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(add_help=True)
     subparsers = parser.add_subparsers()
+
     subparser = subparsers.add_parser("build")
     _ = subparser.add_argument("target", action="extend", nargs="*", type=str)
     subparser.set_defaults(func=do_build)
+
     subparser = subparsers.add_parser("iso")
     _ = subparser.add_argument("target", action="extend", nargs="*", type=str)
     subparser.set_defaults(func=do_iso)
+
+    subparser = subparsers.add_parser("rootfs")
+    now = datetime.now()
+    _ = subparser.add_argument("--year", default=now.year)
+    _ = subparser.add_argument("--month", default=now.strftime("%m"))
+    _ = subparser.add_argument("--day", default=now.strftime("%d"))
+    _ = subparser.add_argument("--tag", default=None)
+    _ = subparser.add_argument(
+        "--no-build",
+        action="store_true",
+        dest="noBuild"
+    )
+    subparser.set_defaults(func=do_rootfs)
+
     args = parser.parse_args()
     if not hasattr(args, "func"):
         parser.print_help()
-        sys.exit(1)
-
-    if not is_root():
-        print("Must be run as root")
         sys.exit(1)
 
     cast(Callable[[argparse.Namespace], None], args.func)(args)
--- variants/base.Containerfile
+++ variants/base.Containerfile
@@ -1,55 +1,5 @@
 #syntax=docker/dockerfile:1.4
-ARG ARCHIVE_YEAR=2025
-ARG ARCHIVE_MONTH=05
-ARG ARCHIVE_DAY=01
-ARG PACSTRAP_TAG=20250427.0.341977
-
-FROM docker.io/library/archlinux:base-devel-${PACSTRAP_TAG} AS pacstrap
-
-ARG ARCHIVE_YEAR
-ARG ARCHIVE_MONTH
-ARG ARCHIVE_DAY
-
-RUN \
-  echo "Server = https://archive.archlinux.org/repos/${ARCHIVE_YEAR}/${ARCHIVE_MONTH}/${ARCHIVE_DAY}/\$repo/os/\$arch" > /etc/pacman.d/mirrorlist \
-  && echo "Server = https://america.archive.pkgbuild.com/repos/${ARCHIVE_YEAR}/${ARCHIVE_MONTH}/${ARCHIVE_DAY}/\$repo/os/\$arch" >> /etc/pacman.d/mirrorlist \
-  && echo "Server = https://asia.archive.pkgbuild.com/repos/${ARCHIVE_YEAR}/${ARCHIVE_MONTH}/${ARCHIVE_DAY}/\$repo/os/\$arch" >> /etc/pacman.d/mirrorlist \
-  && echo "Server = https://europe.archive.pkgbuild.com/repos/${ARCHIVE_YEAR}/${ARCHIVE_MONTH}/${ARCHIVE_DAY}/\$repo/os/\$arch" >> /etc/pacman.d/mirrorlist
-RUN pacman-key --init \
-  && pacman -Sy --needed --noconfirm archlinux-keyring moreutils
-RUN mkdir /rootfs
-WORKDIR /rootfs
-RUN mkdir -m 0755 -p var/{cache/pacman/pkg,lib/pacman,log} dev run etc \
-  && mkdir -m 1777 tmp \
-  && mkdir -m 0555 sys proc
-RUN chronic fakeroot pacman -r . -Sy --noconfirm base mkinitcpio moreutils
-RUN rm usr/share/libalpm/hooks/60-mkinitcpio-remove.hook \
-  && rm usr/share/libalpm/hooks/90-mkinitcpio-install.hook \
-  && cp -a {/,}etc/pacman.d/mirrorlist \
-  && cp -a {/,}etc/pacman.conf
-
-FROM scratch AS base
-
-WORKDIR /
-COPY --from=pacstrap /rootfs /
-COPY overlay/pacstrap /
-ENTRYPOINT [ "/bin/bash" ]
-
-ARG ARCHIVE_YEAR
-ARG ARCHIVE_MONTH
-ARG ARCHIVE_DAY
-
-RUN <<EOF cat > /etc/os-release
-NAME="Atomic Arch"
-PRETTY_NAME="Atomic Arch Linux"
-ID=atomic-arch
-HOME_URL="https://github.com/Eeems/atomic-arch"
-SUPPORT_URL="https://github.com/Eeems/atomic-arch/issues"
-BUG_REPORT_URL="https://github.com/Eeems/atomic-arch/issues"
-VERSION_ID=${ARCHIVE_YEAR}.${ARCHIVE_MONTH}.${ARCHIVE_DAY}
-VARIANT=Base
-VARIANT_ID=base
-EOF
+FROM atomic-arch:rootfs as base
 
 RUN /usr/lib/system/install_packages \
   base \
