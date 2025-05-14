eef2972d52e72cb29300241dbb181b2ad9c7c31e
Nathaniel van Diepen <nathaniel.vandiepen@gmail.com>
2025-05-13 18:13:56 -0600

    Cleanup and fix session-setup

--- overlay/atomic/usr/lib/system/_os/cli/session-setup.py
+++ overlay/atomic/usr/lib/system/_os/cli/session-setup.py
@@ -1,30 +1,31 @@
 import os
 
-from collections.abc import Iterable
 from argparse import ArgumentParser
 from argparse import Namespace
-from typing import cast
 
-from .. import system
+from ..niri import getOutputs
+from ..niri import setOutputScale
+from ..niri import setVolumeOut
+from ..niri import getVolumeOut
+from ..niri import setVolumeIn
+from ..niri import getVolumeIn
 
 
 def register(_: ArgumentParser):
     pass
 
 
-def command(_: Namespace):
+def command(args: Namespace):  # pyright:ignore [reportUnusedParameter]
     # TODO load settings from a config file
-    for display in cast(
-        Iterable[str],
-        system.getOutputs().keys(),  # pyright:ignore [reportUnknownMemberType]
-    ):
-        system.setOutputScale(display, 100)
+    for display in getOutputs().keys():
+        _ = setOutputScale(display, 100)
 
     # Ensure volumes are clipped to the max allowed
-    system.setVolumeOut(system.getVolumeOut())
-    system.setVolumeIn(system.getVolumeIn())
-    if not os.path.exists("~/.config/waypaper/config.ini"):
-        os.symlink("/usr/share/waypaper/config.ini", "~/.config/waypaper/config.ini")
+    _ = setVolumeOut(getVolumeOut())
+    _ = setVolumeIn(getVolumeIn())
+    config = os.path.expanduser("~/.config/waypaper/config.ini")
+    if not os.path.exists(config):
+        os.symlink("/usr/share/waypaper/config.ini", config)
 
 
 if __name__ == "__main__":
