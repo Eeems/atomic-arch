68d9bf3b2a347b2cc0159b4038d0c85c2cb01ffe
Nathaniel van Diepen <nathaniel.vandiepen@gmail.com>
2025-11-17 12:56:56 -0700

    Speed up status command

--- overlay/base/usr/lib/system/_os/cli/status.py
+++ overlay/base/usr/lib/system/_os/cli/status.py
@@ -1,5 +1,6 @@
 from argparse import ArgumentParser
 from argparse import Namespace
+from concurrent.futures import ThreadPoolExecutor
 
 from .. import OS_NAME
 
@@ -11,35 +12,39 @@ def register(_: ArgumentParser):
     pass
 
 
+def get_status(data: tuple[int, str, str]) -> str:
+    index, checksum, type = data
+    with open(
+        f"/ostree/deploy/{OS_NAME}/deploy/{checksum}/usr/lib/os-release", "r"
+    ) as f:
+        osInfo = {
+            x[0]: x[1]
+            for x in [
+                x.strip().split("=", 1)
+                for x in f.readlines()
+                if x.startswith("VERSION_ID=")
+                or x.startswith("VERSION=")
+                or x.startswith("BUILD_ID=")
+            ]
+        }
+
+    ref = baseImage(f"/ostree/deploy/{OS_NAME}/deploy/{checksum}/etc/system/Systemfile")
+    version = osInfo.get("VERSION", "0")
+    version_id = osInfo.get("VERSION_ID", "0")
+    build_id = osInfo.get("BUILD_ID", "0")
+    status = f"{index}: {ref}"
+    if type:
+        status += f" ({type})"
+
+    status += f"\n  Version: {version}.{version_id}"
+    status += f"\n  Build:   {build_id}"
+    return status
+
+
 def command(_: Namespace):
-    for index, checksum, type in deployments():
-        with open(
-            f"/ostree/deploy/{OS_NAME}/deploy/{checksum}/usr/lib/os-release", "r"
-        ) as f:
-            osInfo = {
-                x[0]: x[1]
-                for x in [
-                    x.strip().split("=", 1)
-                    for x in f.readlines()
-                    if x.startswith("VERSION_ID=")
-                    or x.startswith("VERSION=")
-                    or x.startswith("BUILD_ID=")
-                ]
-            }
-
-        ref = baseImage(
-            f"/ostree/deploy/{OS_NAME}/deploy/{checksum}/etc/system/Systemfile"
-        )
-        print(f"{index}: {ref}", end="")
-        if type:
-            print(f" ({type})", end="")
-
-        print()
-        version = osInfo.get("VERSION", "0")
-        version_id = osInfo.get("VERSION_ID", "0")
-        print(f"  Version: {version}.{version_id}")
-        build_id = osInfo.get("BUILD_ID", "0")[:19]
-        print(f"  Build:   {build_id}")
+    with ThreadPoolExecutor(max_workers=50) as exc:
+        for status in exc.map(get_status, deployments()):
+            print(status)
 
 
 if __name__ == "__main__":
